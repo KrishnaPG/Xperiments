c1bedd5e172dcbd4d51d69b6d557fff76667705a
Gopalakrishna Palem <KrishnaPG@Yahoo.com>
2018-09-19 18:52:35 +0530

    facing nonce issue

--- bcdb-cc/src/App.vue
+++ bcdb-cc/src/App.vue
@@ -1,17 +1,37 @@
 <template>
   <div id="app">
-    <v-button @click="transfer">Transfer</v-button>
-    <p>Last Transaction: <a :href="lastTransactionURI" target="_blank">{{lastTransactionId}}</a> </p>
+    <!-- <v-button @click="transfer">Transfer</v-button> -->
+    <p>Last Transaction:  Win: {{userWin}}  Loss:{{userLoss}}  Balance: {{accountBalance}}</p>
     <hr>
     <!-- <secure-exchange-ui></secure-exchange-ui> -->
     <tagDice ref="dice" v-on:roll="onRoll"></tagDice>
     <h4 id="result" v-if="rolls.length">Numbers Rolled: <br/>{{rolls}}</h4>
+    <hr>
+    <table>
+      <thead>
+        <tr><th>Outcome</th><th>Odds</th><th>Bet Amount</th></tr>
+      </thead>
+      <tbody>
+        <tr v-for="(name, index) in betNames">
+          <td>{{name}}</td>
+          <td>{{betOdds[index]}}</td>
+          <td>
+            <v-input-number v-model="betAmounts[index]" placeholder="0" value="0"/>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 const tagDice = resolve => require(['@/components/dice'], resolve);
 import secureExchangeUi from '@/components/secure-exchange';
+const ccSDK = new (require("codechain-sdk"))({ server: 'http://101.53.152.47:8080' });
+const bookieAccount = "tccqzn9jjm3j6qg69smd7cn0eup4w7z2yu9my9a2k78";  // built-in test account that has lots of money
+const bookiePwd = "satoshi";
+const userAccount = "tccqzaa3x0jkh7w6lfz3nzrszupawdpxsljlsuv74yc";  // account of the user who is betting
+const userPwd = "password";
 const Buffer = require('buffer').Buffer;
 const BigchainDB = require('bigchaindb-driver');
 const base58 = require('bs58');
@@ -48,11 +68,23 @@ export default {
     return {
       lastTransactionId: null,
       lastTransactionURI: null,
-      rolls: []
+      rolls: [],
+      betNames: ['One', 'Two', 'Three', 'Four', 'Five', 'Six'],
+      betOdds: [],
+      betAmounts: [],
+      userWin: '',
+      userLoss: '',
+      invoices: {},
+      accountBalance: 0,
     }   
   },
   mounted: function()
   {
+    this.refreshOdds();
+    this.refreshBalance();
+
+    return;
+
     console.log(BigchainDB.Transaction.makeEd25519Condition(user1.publicKey))
     // at the output of the transaction to-be-spent
     // Generate threshold condition 2 out of 3
@@ -97,6 +129,12 @@ export default {
       });    */
   },
   methods: {
+    refreshOdds: function() {
+      this.betNames.forEach((name, index) => { 
+        this.betOdds[index] = Math.ceil(Math.random() * 100) / 10; 
+        this.betAmounts[index] = 0;
+      });
+    },
     transfer: function() {
       // PreimageSha256 is anonymous condition. Fulfiller should guess what the condition is. 
       // Same as password. Whoever guesses it correctly can access it.
@@ -121,11 +159,59 @@ export default {
         
         console.log(cc.validateFulfillment(ed25519Fulfillment, ed25519Condition.getConditionUri(), new Buffer('password')));
       }
+      {
+        const account ="tccqzaa3x0jkh7w6lfz3nzrszupawdpxsljlsuv74yc";
+        const password ="password";
+
+        ccSDK.rpc.account.getList().then(accounts => {
+          accounts.forEach(element => {
+            ccSDK.rpc.chain.getBalance(element).then(balance => console.log(`account ${element} has balance: ${balance}`));
+          });
+        });
+
+
+
+        console.log("this: bet: ", this.betAmounts);
+        //console.log("code chain address: ", ccSDK.key.classes.PlatformAddress.fromAccountId(account));
+      }
     },
     onRoll: function(value) {
+      this.betNames.forEach((name, index) => { if(!this.betAmounts[index] || this.betAmounts[index]=="") this.betAmounts[index]  = 0; });
+      const winIndex = value -1; // for zero-based indexing
+      this.userWin = +(this.betAmounts[winIndex] * this.betOdds[winIndex]).toFixed(2);
+      this.userLoss = +(this.betAmounts.reduce((total, amount) => { return total + amount; }) - this.betAmounts[winIndex]).toFixed(2);
+
+      this.transferWinLossAmount();
+
+      // prepare UI for next round
+      // this.refreshOdds();
       if(this.rolls.length >= 5) this.rolls.shift();
       this.rolls.push(value);
-    }  
+    },
+    transferWinLossAmount: function() {
+      if(this.userLoss) { this.sendPayment(userAccount, userPwd, bookieAccount, this.userLoss, Date.now()); console.log("transfer out: ", this.userLoss); }
+      if(this.userWin) { this.sendPayment(bookieAccount, bookiePwd, userAccount, this.userWin, Date.now());    console.log("transfer in: ", this.userWin); }
+    },
+    sendPayment: function(account, passphrase, recipient, amount, refNo) {
+      const parcel = ccSDK.core.createPaymentParcel({ recipient,  amount });
+      ccSDK.rpc.chain.sendParcel(parcel, {
+        account,
+        passphrase,
+        nonce: Date.now()
+      }).then(parcelHash => {
+        return ccSDK.rpc.chain.getParcelInvoice(parcelHash, { timeout: 300 * 1000 });
+      }).then(parcelInvoice => { console.log("invoice: refNo ", refNo);
+        this.invoices[refNo] = parcelInvoice;
+        this.refreshBalance();
+      });
+    },
+    refreshBalance: function() {
+      ccSDK.rpc.chain.getBalance(userAccount).then(balance => {
+        // the balance is a U256 instance at this moment. Use toString() to print it out.
+        this.accountBalance = balance.value;
+        console.log("balance: ", balance.toString()); // the amount of CCC that the account has.
+      });  
+    }
   }
 }
 </script>
