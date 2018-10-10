2fa2d5bd934bc804ef5e39aa38cc123852e0cfbb
KrishnaPG <KrishnaPG@Yahoo.com>
2018-10-10 09:23:53 +0530

    adding asset address

--- bcdb-cc/src/App.vue
+++ bcdb-cc/src/App.vue
@@ -82,6 +82,15 @@ export default {
   mounted: function()
   {
 
+    ccSDK.key
+      .createAssetTransferAddress()
+      .then(function (address) {
+        // This type of address is used to receive assets when minting or transferring them.
+        // Example: tcaqqq9pgkq69z488qlkvhkpcxcgfd3cqlkzgxyq9cewxuda8qqz7jtlvctt5eze
+        console.log("asset transfer address: ", address.toString());
+      })
+      .catch(console.error);    
+
     var secret = "ede1d4ccb4ec9a8bbbae9a13db3f4a7b56ea04189be86ac3a6a439d9a0a1addd";
     var passphrase = "satoshi";
     ccSDK.rpc.account.importRaw(secret, passphrase).then(function (account) {
