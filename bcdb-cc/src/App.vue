ec743ae299dda73ca3088047e66f723f8efdf7cd
Gopalakrishna Palem <KrishnaPG@Yahoo.com>
2018-10-15 11:00:29 +0530

    trying mega threshold condition

--- bcdb-cc/src/App.vue
+++ bcdb-cc/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <v-button @click="transfer">Transfer</v-button>
+    <v-button @click="getConditions">Get Conditions</v-button>
     <p>Last Transaction: <a :href="lastTransactionURI" target="_blank">{{lastTransactionId}}</a> </p>
     <p>Last Transaction:  Win: {{userWin}}  Loss:{{userLoss}}  Balance: {{accountBalance}} <br/>Bookie Balance: {{bookieBalance}}</p>
     <hr>
@@ -38,20 +39,22 @@ const conn = new BigchainDB.Connection(API_PATH /*, {
   app_id: '21049ec1',
   app_key: '79a0c993f29ff4ba921ea724a112f6f6'
 }*/);
-const user1 = new BigchainDB.Ed25519Keypair()
-const user2 = new BigchainDB.Ed25519Keypair()
-const user3 = new BigchainDB.Ed25519Keypair()
-const creator = new BigchainDB.Ed25519Keypair()
-const receiver = new BigchainDB.Ed25519Keypair();
-const alice = new BigchainDB.Ed25519Keypair();
-const bob = new BigchainDB.Ed25519Keypair();
-const carly = new BigchainDB.Ed25519Keypair();
+const user1 = { privateKey: "DTotuBdRBcM1UvC6MtLW2G8gXDLyc4Qs8WEArmf95VMV", publicKey: "J8ZHPqCCcdvgfySPqX882RYK6o1ghYJGkTHSUePY5e1q"}; //new BigchainDB.Ed25519Keypair()
+const user2 = { privateKey: "J8RyFH7UKxvo7eiejQkSiF5kWEbFyiX7vA4GuUTU8pTL", publicKey: "Dd4fBiWUe9FUC8WA53qmYcf4icW59jaaMDCXGDNiyqen"};
+const user3 = { privateKey: "6ixNmhdEGkodWNsE2tYUTQDzTMcykwswhaa2hunaNudB", publicKey: "8Y4QQ2aQYuVV6eVRow8Kw5F4AnrsUjRdg1UjwME2m5ZL"};
+const creator = { privateKey: "BQCwFVfU7BvPC1P9SYwTaSwwWfiVCzur6JKUZNs3m6nU", publicKey: "9veanEQsymeJRbwmZRhQZ1Hziyid1ruA9XBsZqkbuy5L"};
+const receiver = { privateKey: "CH7Stqqftmtd2i8NipTFVj73HNuNa1JvnFdSmSCkWXWw", publicKey: "Gw1Qkyj1wrQKj59niRpW2rWKZMp5HcnoDD2ie6iHi5Xk"};
+const alice = { privateKey: "7WrTwuAEGS2iDBaEqHxx9CFrR3TBmXZESTXyigoU7xCa", publicKey: "BWX6F78efHycQfU7VLKGGNzhc3GMxkwmwqedbBYJc3fY"};
+const bob = { privateKey: "2bupkLmrgWfkZXrsFao2oDUDemQf7GdpTgPa71w811KE", publicKey: "21GuzGZ1tSwyvEMN93YpJCXe5H9GdEdbvYzNhjMHbLPX"};
+const carly = { privateKey: "HHtvWosRJ4uFby4KcVPASFjRdUqgHs8r5DQCuYc6oWAF", publicKey: "7qnh1UBqVXz2YSNhuuT1mDZZUPxxx7r9VVdVn2qxz8Sp"};
 const threshold = 2
 const condition1 = BigchainDB.Transaction.makeEd25519Condition(user1.publicKey, false)
 const condition2 = BigchainDB.Transaction.makeEd25519Condition(user2.publicKey, false)
 const condition3 = BigchainDB.Transaction.makeEd25519Condition(user3.publicKey, false)
+const condition4 = BigchainDB.Transaction.makeEd25519Condition(carly.publicKey, false);
 
-const thresholdCondition = BigchainDB.Transaction.makeThresholdCondition(threshold, [condition1, condition2, condition3]);
+const thresholdCondition = BigchainDB.Transaction.makeThresholdCondition(threshold, [condition1, condition2, condition3]);  // 2 of {user1, user2, user3}
+const megaCondition = BigchainDB.Transaction.makeThresholdCondition(threshold, [thresholdCondition, condition4]); // both {Carly, and 2 of {user1, user2, user3}}
 let txCreateAliceSimpleSigned;
 let txSigned;
 export default {
@@ -81,9 +84,10 @@ export default {
     // at the output of the transaction to-be-spent
     // Generate threshold condition 2 out of 3
 
-    console.log(thresholdCondition);
-    let output = BigchainDB.Transaction.makeOutput(thresholdCondition);
-    output.public_keys = [user1.publicKey, user2.publicKey, user3.publicKey];
+    console.log("thresholdCondition: ", thresholdCondition);
+    console.log("mega condition: ", megaCondition);
+    let output = BigchainDB.Transaction.makeOutput(megaCondition);
+    output.public_keys = [user1.publicKey, user2.publicKey, user3.publicKey, carly.publicKey];
 
     const tx = BigchainDB.Transaction.makeCreateTransaction(
       {
@@ -134,6 +138,12 @@ export default {
         this.betAmounts[index] = 0;
       });
     },
+    getConditions: function() {
+      if(!this.lastTransactionId) return;
+      conn.getTransaction(this.lastTransactionId).then(res => {
+        console.log("transaction: ", res);
+      }).catch(console.error);
+    },
     sha256Hash: function(data) {
       return sha3.sha3_256
         .create()
@@ -178,9 +188,22 @@ export default {
         fulfillment.addSubfulfillment(fulfillment2.serializeUri())
         fulfillment.addSubconditionUri(condition3.getConditionUri())
 
+        
+        let fulfillment4 = BigchainDB.Transaction.makeEd25519Condition(carly.publicKey, false)
+        const transactionUniqueFulfillment4 = createTranfer.inputs[0].fulfills ? serializedTransaction
+          .concat(createTranfer.inputs[0].fulfills.transaction_id)
+          .concat(createTranfer.inputs[0].fulfills.output_index) : serializedTransaction
+        const transactionHash4 = this.sha256Hash(transactionUniqueFulfillment4)
+        fulfillment4.sign(Buffer.from(transactionHash4, 'hex'), new Buffer.from(base58.decode(carly.privateKey)));
+
+        let megaFulfillment = new cryptoconditions.ThresholdSha256();
+        megaFulfillment.threshold = 2;
+        megaFulfillment.addSubfulfillment(fulfillment.serializeUri());
+        megaFulfillment.addSubfulfillment(fulfillment4.serializeUri());
+
         //Sign the transaction
         const fulfillmentUri = fulfillment.serializeUri()
-        createTranfer.inputs[0].fulfillment = fulfillmentUri
+        createTranfer.inputs[0].fulfillment = megaFulfillment.serializeUri()//fulfillmentUri
 
         createTranfer.id = await this.sha256Hash(BigchainDB.Transaction.serializeTransactionIntoCanonicalString(createTranfer))
 
